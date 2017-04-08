74cec08e56e98423385c08a0e0da562933cca93b
mahikaw <mahika16241@iiitd.ac.in>
2017-04-08 14:38:26 +0530

    basic activity

--- app/src/main/java/com/example/mahika/smart_backpack/MainActivity.java
+++ app/src/main/java/com/example/mahika/smart_backpack/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -17,68 +18,70 @@ import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN =2 ;
-    private static final String TAG ="xyz" ;
-    private GoogleApiClient mGoogleApiClient;
-    private BreakIterator mStatusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void signIn(View view){
+        Intent intent = new Intent(this, home_activity.class);
+        startActivity(intent);
+    }
         // Configure sign-in to request the user's ID, email address, and basic
 // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-// options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
-        findViewById(R.id.sign_in_button).setOnClickListener((View.OnClickListener) this);
-    }
-
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-            // ...
-        }
-    }
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-    private void handleSignInResult(GoogleSignInResult result) {
-        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
-            updateUI(true);
-        } else {
-            // Signed out, show unauthenticated UI.
-            updateUI(false);
-        }
-    }
-
-    private void updateUI(boolean b) {
-//        if(b==true)
-//            AuthenticatedUI
-    }
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestEmail()
+//                .build();
+//
+//        // Build a GoogleApiClient with access to the Google Sign-In API and the
+//// options specified by gso.
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+//                .build();
+//        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+//        signInButton.setSize(SignInButton.SIZE_STANDARD);
+//        findViewById(R.id.sign_in_button).setOnClickListener((View.OnClickListener) this);
+//    }
+//
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.sign_in_button:
+//                signIn();
+//                break;
+//            // ...
+//        }
+//    }
+//
+//    private void signIn() {
+//        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+//    }
+////    @Override
+////    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+////        super.onActivityResult(requestCode, resultCode, data);
+////
+////        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+////        if (requestCode == RC_SIGN_IN) {
+////            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+////            handleSignInResult(result);
+////        }
+////    }
+////    private void handleSignInResult(GoogleSignInResult result) {
+////        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+////        if (result.isSuccess()) {
+////            // Signed in successfully, show authenticated UI.
+////            GoogleSignInAccount acct = result.getSignInAccount();
+////            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+////            updateUI(true);
+////        } else {
+////            // Signed out, show unauthenticated UI.
+////            updateUI(false);
+////        }
+////    }
+////
+////    private void updateUI(boolean b) {
+//////        if(b==true)
+//////            AuthenticatedUI
+////    }
 }
